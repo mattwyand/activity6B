b2c2eb825148fe9cbef9cedbf9af0e32e9282034
carpetbakery <20468196+CarpetBakery@users.noreply.github.com>
2025-02-13 21:52:14 -0500

    fulfilled request

--- nevercompile.cpp
+++ nevercompile.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+void printIt()
+{
+    for (int i=0; i<1000; i++)
+    {
+        printf("supermanberdini %d\n", i);
+    }
+}
+
 int main()
 {
-    printf("Running");
+    printf("Running\n");
+
+    printIt();
+    printIt();
 
     return 0;
 }
